c98a90a9be73c86720ed319d297c84769efbb0e4
harold <harin759@gmail.com>
2023-08-07 03:10:41 -0500

    addTestProduct

--- cypress/e2e/shoppingCart.cy.js
+++ cypress/e2e/shoppingCart.cy.js
@@ -1,27 +1,18 @@
 describe("Test Shopping Cart", () => {
 
-    beforeEach(() => {
+    before(() => {
         cy.visit("https://www.saucedemo.com/")
         cy.get("#user-name").type("standard_user")
         cy.get("#password").type("secret_sauce")
         cy.get("#login-button").click()
     })
 
-    it("Link Twiter",() => {
+    it("Agregar producto",() => {
 
-        cy.get(".social_twitter > a").click()
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('.shopping_cart_badge').should("be.visible")
+        
 
-    })
-
-    it("Link Facebook",() => {
-
-        cy.get(".social_facebook > a").click()
-
-    })
-
-    it("Link Linkedin",() => {
-
-        cy.get(".social_linkedin > a").click()
     })
 
 
